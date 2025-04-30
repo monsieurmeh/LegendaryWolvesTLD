c890d33676e33e2df45a28fd1d8b6ef47c347424
MonsieurMeh <monsieurmeh@devnullsoftware.com>
2025-04-29 23:22:51 -0700

    0.0.2

---
+++ Patches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+
+
+namespace MonsieurMeh.Mods.TLD.LegendaryWolves
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(SpawnRegion), "Spawn", new Type[] { typeof(WildlifeMode) })]
+        internal class SpawnRegion_SpawnPatch
+        {
+            public static void Prefix()
+            {
+                LegendaryWolvesManager.Instance?.Log($"Starting SpawnRegion.Spawn!\n{UnityEngine.StackTraceUtility.ExtractStackTrace()}");
+            }
+
+
+            public static void Postfix()
+            {
+                LegendaryWolvesManager.Instance?.Log($"Finishing call of SpawnRegion.Spawn!\n{UnityEngine.StackTraceUtility.ExtractStackTrace()}");
+            }
+        }
+    }
+}
