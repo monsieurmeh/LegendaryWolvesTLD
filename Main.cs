9c70d33dd331008f8618c6e22e8dcf2f1e6f9505
MonsieurMeh <monsieurmeh@devnullsoftware.com>
2025-04-29 23:24:25 -0700

    0.0.2

--- Main.cs
+++ Main.cs
@@ -1,7 +1,7 @@
 ﻿using MelonLoader;
 
-// 0.0.1: Setup
-[assembly: MelonInfo(typeof(MonsieurMeh.Mods.TLD.LegendaryWolves.Main), "LegendaryWolves", "0.0.1", "MonsieurMeh", null)]
+
+[assembly: MelonInfo(typeof(MonsieurMeh.Mods.TLD.LegendaryWolves.Main), "LegendaryWolves", "0.0.2", "MonsieurMeh", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
 
 namespace MonsieurMeh.Mods.TLD.LegendaryWolves
